99fa40d637d22ef1819c2ad916ece9b394b8840a
jose cassio dos santos <88626989+JoseCassioSantos@users.noreply.github.com>
2021-11-04 22:19:28 -0300

    att

---
+++ Script/user.js
@@ -0,0 +1,12 @@
+function btnPerfil() {
+
+    window.location.replace ('index.html');
+}
+
+function btnPadd() {
+    alert('Assine o GracifliX Pro para ter mais perfils!!!')
+}
+
+function btnPger() {
+    alert('Você tem apenas um perfil !')
+}
