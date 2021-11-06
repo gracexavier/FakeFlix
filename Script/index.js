abc5fe4bac318efd7e3b1158113c09f5b90f9cfc
jose cassio dos santos <88626989+JoseCassioSantos@users.noreply.github.com>
2021-11-06 17:25:41 -0300

    almost there
    Ufa
    
    Co-Authored-By: Mateus Gomes <44241134+mateusfranca97@users.noreply.github.com>
    Co-Authored-By: Graciele Xavier <61364633+gracexavier@users.noreply.github.com>

--- Script/index.js
+++ Script/index.js
@@ -7,3 +7,20 @@ function scrollFunction() {
     document.getElementById("header").style.background = "linear-gradient(180deg, rgb(12, 12, 12) 10%, rgba(0, 0, 0, 0) 100%";
   }
 }
+
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
